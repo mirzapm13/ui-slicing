939425029d01339e26e582ea9af181021a7706c8
mirzapm13 <mirzapm13@gmail.com>
2022-12-21 16:34:24 +0700

    responsive modal

--- src/components/atoms/Button/index.jsx
+++ src/components/atoms/Button/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './_button.scss';
 
-export default function Button({ text, click }) {
+export default function Button({ text, click, addClass }) {
   return (
     <div className="border-gradient">
       <div className="border-transparent">
-        <button className="button" type="button" onClick={click}>
+        <button className={`button ${addClass}`} type="button" onClick={click}>
           {text}
         </button>
       </div>
--- src/components/molecules/Navigation/index.jsx
+++ src/components/molecules/Navigation/index.jsx
@@ -3,6 +3,8 @@ import './_navigation.scss';
 import ContactModal from 'components/organisms/ContactModal';
 import Check from 'images/check.svg';
 import Dropdown from 'images/dropdown-toggle.png';
+import Contact from 'images/contact-us.png';
+import { useState } from 'react';
 import useContactModal from '../../../hooks/useContactModal';
 import CommonModal from '../../organisms/CommonModal';
 import useCommonModal from '../../../hooks/useCommonModal';
@@ -10,6 +12,7 @@ import useCommonModal from '../../../hooks/useCommonModal';
 export default function Navigation() {
   const { isShowing, toggle } = useContactModal();
   const { isShowing: commonModalShowing, toggle: commonModalToggle } = useCommonModal();
+  const [dropdown, setDropdown] = useState(false);
 
   const submitHandler = () => {
     toggle();
@@ -18,18 +21,30 @@ export default function Navigation() {
 
   return (
     <>
-      <nav>
+      <nav className="normal">
         <ul>
           <li className="list-item">How It Works</li>
           <li className="list-item">How To Get It</li>
           <li className="list-item">Testimonials</li>
           <li className="list-icon">
-            <img src={Dropdown} alt="dropdown-toggle-button" />
+            <img
+              src={Dropdown}
+              alt="dropdown-toggle-button"
+              onClick={() => setDropdown(!dropdown)}
+              role="presentation"
+            />
           </li>
           <li className="list-button">
-            <Button text="Contact Us" click={() => toggle()} />
+            <Button text="Contact Us" click={toggle} addClass="desktop-button" />
+            <img src={Contact} alt="" onClick={toggle} role="presentation" className="mobile-button" />
           </li>
         </ul>
+      </nav>
+
+      <nav className={`dropdown${dropdown ? ' active' : ''}`}>
+        <a href="/">How It Works</a>
+        <a href="/">How To Get It</a>
+        <a href="/">Testimonial</a>
       </nav>
 
       <ContactModal isShowing={isShowing} hide={toggle} btnClick={submitHandler} />
